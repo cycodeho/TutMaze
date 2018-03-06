6148d4340d1a3d4940b3f45cfa34f8b11066ab12
cycodeho <hocyalbert@gmail.com>
2018-03-06 21:36:49 +0800

    1st commit

---
+++ Assets/GunScript.cs
@@ -0,0 +1,43 @@
+﻿using Mono.CSharp;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire1");
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        Debug.Log("EnterShoot");
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log("Shoot:" + hit.transform.name);
+
+            TargetScript target = hit.transform.GetComponent<TargetScript>();
+            if(target != null)
+            {
+                Debug.Log("Damage:" + damage);
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/TargetScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour {
+
+    public float health = 50f;
+    
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+
+    public void TakeDamage(float amount)
+    {
+        Debug.Log("Current HP:" + amount);
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
