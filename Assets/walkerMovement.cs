1944ccd3d697e66391f6f8060d095c289f33b6d3
Sam <neiamenase@gmail.com>
2018-03-11 20:39:33 +0800

    add zombie AI - 1

--- Assets/walkerMovement.cs
+++ Assets/walkerMovement.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class walkerMovement : MonoBehaviour {
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+	GameObject walker;
+	public float fieldOfView= 100f;
+	Animator animator;
+	private SphereCollider col;
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+		walker = GameObject.FindGameObjectWithTag ("Enermy");
+		animator = GetComponent<Animator>();
+		col = GetComponent<SphereCollider> ();
+	}
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +26,18 @@ public class walkerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 direction = player.transform.position - transform.position;
+		float angle = Vector3.Angle (direction, transform.forward);
+
+		if (angle < fieldOfView * 0.5f) {
+			RaycastHit hit;
+			Debug.DrawRay(transform.position + transform.up * 3, transform.forward * 1000f, Color.blue, Time.deltaTime, true);
+			if (Physics.Raycast (transform.position + transform.up * 3, direction.normalized, out hit, col.radius)) {
+				if (hit.collider.gameObject == player) {
+					animator.SetBool ("isSeen", true);
+					nav.SetDestination (player.transform.position);
+				}
+			}
+		}
 	}
 }
